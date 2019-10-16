e7628aef5117baacae6983bc35cfe61f8569306c
REdrsnow <1330214954@qq.com>
2019-10-17 02:33:37 +0800

    创建后端项目

---
+++ Painting-PhotographyLifeWebServer/src/main/java/cn/adam/website/paintingphotographylifewebserver/PaintingPhotographyLifeWebServerApplication.java
@@ -0,0 +1,13 @@
+package cn.adam.website.paintingphotographylifewebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingPhotographyLifeWebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintingPhotographyLifeWebServerApplication.class, args);
+    }
+
+}
