ba3c1ed9bc5c55c6fa714acef96f2b432db06573
REdrsnow <1330214954@qq.com>
2019-10-18 16:58:08 +0800

    加入vuex

--- Painting-PhotographyLifeWeb/src/App.vue
+++ Painting-PhotographyLifeWeb/src/App.vue
@@ -1,30 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
-    </div>
+	  
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
-    }
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: Helvetica, sans-serif;
-  text-align: center;
+	width: 300px;
+	height: 200px;
+	background-color: aliceblue;
+	border-radius: 10px;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
 }
 </style>
--- Painting-PhotographyLifeWeb/src/main.js
+++ Painting-PhotographyLifeWeb/src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import Vuex from 'vuex'
+import './assets/css/main.css'
 
+Vue.use(Vuex)
 Vue.use(ElementUI)
+
+const store = new Vuex.Store({
+	state: {
+		test: "test"
+	}
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  store
 })
