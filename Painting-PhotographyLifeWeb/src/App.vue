c68757a09b37a34f5a3245d5acca98c4548f49f8
REdrsnow <1330214954@qq.com>
2019-10-21 00:03:24 +0800

    更新首页

--- Painting-PhotographyLifeWeb/src/App.vue
+++ Painting-PhotographyLifeWeb/src/App.vue
@@ -1,35 +1,33 @@
 <template>
   <div id="app">
-	  <el-row :gutter="20" type="flex" justify="center">
-		  <transition name="el-zoom-in-center">
-			  <el-col :sm="15">
-				  <h1 class="title">绘影生活</h1>
-				  <p>
-					  aesf qwr gwrga 
-					  erg aergq awhrqa whqeht
-					  qae hqrhqr hqerg qwre hgqwrhg qer herh
-				  </p>
-			  </el-col>
-		  </transition>
-		  <transition name="el-zoom-in-center">
-			  <el-col :sm="9" :xs="0" class="col">
-				  <Login></Login>
-			  </el-col>
-		  </transition>
+	  <el-row :gutter="20" type="flex" justify="center" align="middle">
+		  <el-col :sm="15">
+			  <h1 class="title">绘影生活</h1>
+			  <p class="titlecontext">
+				  绘影生活是一个可以供您发布分享您的绘画或摄影创作的平台，您可以通过该平台结识更多共同爱好者，并与他们一起分享讨论。
+			  </p>
+			  
+			  <el-row type="flex" justify="center">
+			  	<el-col :xs="12" class="col">
+					<el-button class="mybtn">立即进站</el-button>
+			  	</el-col>
+			  	<el-col :xs="12" :sm="0" class="col">
+			  		<el-button class="mybtn"  @click="dialogFormVisible = true">
+			  			登录
+			  			<i class="el-icon-arrow-right"></i>
+			  		</el-button>
+			  	</el-col>
+			  </el-row>
+		  </el-col>
+		  <el-col :sm="9" :xs="0" class="col">
+			  <Login></Login>
+		  </el-col>
 		  
 	  </el-row>
 	  
-		<el-row type="flex" justify="center">
-			<el-col :sm="0" class="col">
-				<el-button class="changebut" size="medium" @click="dialogFormVisible = true">
-					登录页
-					<i class="el-icon-arrow-right"></i>
-				</el-button>
-			</el-col>
-		</el-row>
 		
 		<el-dialog title="登录" :visible.sync="dialogFormVisible" 
-				fullscreen="true" 
+				:fullscreen="true" 
 				custom-class="dialog"
 				append-to-body>
 					  <Login></Login>
@@ -39,24 +37,25 @@
 
 <script>
 	import Login from './component/Login.vue'
-export default {
-	components:{
-		Login
-	},
-	data(){
-		return {
-			dialogFormVisible: false
+	export default {
+		components:{
+			Login
+		},
+		data(){
+			return {
+				dialogFormVisible: false
+			}
 		}
-	},
-	computed:{
-		
-	},methods:{
-		
 	}
-}
 </script>
 
 <style>
+	.titlecontext{
+		font-size: 20px;
+		font-weight: 400;
+		color: hsla(0,0%,100%,.6);
+		text-indent: 2rem;
+	}
 	.dialog{
 		background-color: transparent;
 	}
@@ -67,31 +66,23 @@ export default {
 		
 	}
 
-/* 	.el-input{
-		min-width: 120px;
-	} */
 	.title{
 		font-size: 64px;
 		display: block;
-		/* text-align: center; */
-		text-align: left;
+		text-align: center;
 		font-weight: 500;
 		line-height: 64px;
 		color: #EBEEF5;
-		font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+		/* font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif; */
 	}
 	#app {
-		/* width: 300px;
-		height: 200px; */
 		background-color: transparent;
-		border-radius: 10px;
 		position: absolute;
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
-		/* box-sizing: border-box; */
 		width: 70%;
 		max-width: 1020px;
-		/* opacity: 0.5; */
+		    font-family: "Microsoft YaHei", "Microsoft Sans Serif", "Microsoft SanSerf", "\5FAE\8F6F\96C5\9ED1";
 	}
 </style>
--- Painting-PhotographyLifeWeb/src/component/Login.vue
+++ Painting-PhotographyLifeWeb/src/component/Login.vue
@@ -1,11 +1,32 @@
 <template>
-	<el-form label-width="60px" label-position="top" class="form">
+	<el-form size="mini" label-width="60px" label-position="top" class="form">
 	  <el-form-item label="账号:">
 		<el-input size="medium" class="aaa"></el-input>
 	  </el-form-item>
 	  <el-form-item label="密码:">
 		  <el-input show-password></el-input>
 	  </el-form-item>
+	  
+	  <el-form-item label="验证码:">
+		<el-row :gutter="20" type="flex">
+			<el-col :span="12">
+				<el-input ></el-input>
+			</el-col>
+			<el-col :span="12">
+					<el-image
+						style="width: 75px; height: 29px"
+						:src= "imgurl" 
+						fit="fill" 
+						title="点击图片刷新"></el-image>
+			</el-col>
+		</el-row>
+		</el-form-item>
+		<el-form-item>
+				  <el-button type="primary" class="but">登录</el-button>
+		</el-form-item>
+		<el-form-item>
+				  <el-button class="but">创建</el-button>
+		</el-form-item>
 	</el-form>
 </template>
 
@@ -13,17 +34,23 @@
 	export default{
 		data(){
 			return {
-				
+				imgurl: 'https://static.oschina.net/uploads/img/201712/16113708_B8Hu.png'
 			}
 		}
 	}
 </script>
 
 <style>
+	.but{
+		display: block;
+		width: 100%;
+		margin: 0 auto;
+	}
 	.form{
-		background-color: #EBEEF5;
-		max-width: 250px;
-		min-width: 150px;
+		/* background-color: #EBEEF5; */
+		background-color: rgba(235,238,245, .7);
+		max-width: 300px;
+		min-width: 200px;
 		padding: 24px;
 		border-radius: 4px;
 		box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
